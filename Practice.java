0e18171fb9472247282295800f201b3787e3fd2d
klakkima <lakkimaradkarthik@gmial.com>
2024-09-16 17:48:29 +0530

    second

---
+++ Practice.java
@@ -0,0 +1,9 @@
+public class Practice{
+    public static void main(String[] args) {
+        System.out.println("Kartik");
+        System.out.println("lakkimarad");
+        System.out.print("capgemini ");
+        System.out.print("Technology");
+        System.out.print("Technologies");
+    }
+}
